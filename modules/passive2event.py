05c964b8378fb842b6bcf56c0e9a387322a13d72
Katarina Zailac <katarina.zailac@gmail.com>
2023-10-23 15:04:04 +0200

    Method that creates data to send to Sensu backend to create an event

--- modules/passive2event.py
+++ modules/passive2event.py
@@ -1,6 +1,9 @@
 class Passive2Event:
-    def __init__(self, filename):
+    def __init__(self, filename, metricprofiles, voname, namespace):
         self.filename = filename
+        self.metricprofiles = metricprofiles
+        self.voname = voname
+        self.namespace = namespace
 
     def _parse(self):
         with open(self.filename, "r") as f:
@@ -19,3 +22,56 @@ class Passive2Event:
             })
 
         return data
+
+    def _metric_name(self, metric):
+        if metric.endswith(f"-{self.voname}"):
+            n = -(len(self.voname) + 1)
+            metric = metric[:n]
+
+        return metric
+
+    def _servicetypes4metric(self, metric):
+        metric = self._metric_name(metric)
+
+        servicetypes = set()
+        for metricprofile in self.metricprofiles:
+            for service in metricprofile["services"]:
+                if metric in service["metrics"]:
+                    servicetypes.add(service["service"])
+
+        return sorted(list(servicetypes))
+
+    def _create_event(self, item):
+        servicetypes = self._servicetypes4metric(item["metric"])
+
+        events = list()
+
+        for servicetype in servicetypes:
+            entity_name = f"{servicetype}__{item['hostname']}"
+            metric_name = self._metric_name(item["metric"])
+
+            events.append({
+                "entity": {
+                    "entity_class": "proxy",
+                    "metadata": {
+                        "name": entity_name,
+                        "namespace": self.namespace
+                    }
+                },
+                "check": {
+                    "output": item["output"],
+                    "status": item["status"],
+                    "metadata": {
+                        "name": metric_name
+                    }
+                },
+                "pipelines": [
+                    {
+                        "name": "hard_state",
+                        "type": "Pipeline",
+                        "api_version": "core/v2"
+                    }
+                ]
+            })
+
+        return events
--- tests/test_passive2event.py
+++ tests/test_passive2event.py
@@ -13,12 +13,72 @@ PASSIVE_DATA = """
 PASSIVE_FILE_NAME = "nagios-test.cmd"
 
 
+METRICPROFILES = [
+    {
+        "id": "xxxxx",
+        "date": "2023-09-11",
+        "name": "ARGO_MON_INTERNAL",
+        "description": "Metric profile containing internal metrics.",
+        "services": [
+            {
+                "service": "argo.mon",
+                "metrics": [
+                    "argo.ams-publisher.mon",
+                    "argo.ncg.mon",
+                    "argo.oidc.refresh-token-validity",
+                    "argo.oidc.token-fetch",
+                    "argo.poem-tools.check",
+                    "generic.dirsize.ams-publisher",
+                    "generic.disk.usage-local",
+                    "generic.file.nagios-cmd",
+                    "generic.procs.crond",
+                    "org.nordugrid.ARC-CE-clean",
+                    "org.nordugrid.ARC-CE-monitor",
+                    "srce.cadist.check",
+                    "srce.cadist.get-files",
+                    "srce.certificate.validity-local",
+                    "srce.gridproxy.get",
+                    "srce.gridproxy.validity",
+                    "srce.nagios.good-ses"
+                ]
+            }
+        ]
+    },
+    {
+        "id": "xxxx",
+        "date": "2023-10-19",
+        "name": "ARGO_MON_CRITICAL",
+        "description": "Central ARGO-MON_CRITICAL profile",
+        "services": [
+            {
+                "service": "SRM",
+                "metrics": [
+                    "eu.egi.SRM-CertValidity",
+                    "eu.egi.SRM-GetSURLs",
+                    "eu.egi.SRM-VODel",
+                    "eu.egi.SRM-VOGet",
+                    "eu.egi.SRM-VOGetTurl",
+                    "eu.egi.SRM-VOLs",
+                    "eu.egi.SRM-VOLsDir",
+                    "eu.egi.SRM-VOPut"
+                ]
+            }
+        ]
+    }
+]
+
+
 class Passive2EventTests(unittest.TestCase):
     def setUp(self):
         with open(PASSIVE_FILE_NAME, "w") as f:
             f.write(PASSIVE_DATA)
 
-        self.passive2event = Passive2Event(filename=PASSIVE_FILE_NAME)
+        self.passive2event = Passive2Event(
+            filename=PASSIVE_FILE_NAME,
+            metricprofiles=METRICPROFILES,
+            voname="ops",
+            namespace="TEST"
+        )
 
     def tearDown(self):
         if os.path.isfile(PASSIVE_FILE_NAME):
@@ -56,3 +116,36 @@ class Passive2EventTests(unittest.TestCase):
                 }
             ]
         )
+
+    def test_create_event(self):
+        event = self.passive2event._create_event({
+            "hostname": "grid02.hep.by",
+            "metric": "eu.egi.SRM-VOPut-ops",
+            "status": 0,
+            "output": "OK - File was copied to SRM. Transfer time: "
+                      "0:00:02.863600"
+        })
+        self.assertEqual(event, [{
+            "entity": {
+                "entity_class": "proxy",
+                "metadata": {
+                    "name": "SRM__grid02.hep.by",
+                    "namespace": "TEST"
+                }
+            },
+            "check": {
+                "output": "OK - File was copied to SRM. Transfer time: "
+                          "0:00:02.863600",
+                "status": 0,
+                "metadata": {
+                    "name": "eu.egi.SRM-VOPut"
+                }
+            },
+            "pipelines": [
+                {
+                    "name": "hard_state",
+                    "type": "Pipeline",
+                    "api_version": "core/v2"
+                }
+            ]
+        }])
