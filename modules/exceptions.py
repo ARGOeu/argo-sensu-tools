61eb151225927dfee60c0e75eb5cb3db34637cf2
Katarina Zailac <katarina.zailac@gmail.com>
2023-10-24 10:04:58 +0200

    Class for fetching data from Web-API

--- modules/exceptions.py
+++ modules/exceptions.py
@@ -9,3 +9,8 @@ class ArgoSensuToolsException(Exception):
 class ConfigException(ArgoSensuToolsException):
     def __str__(self):
         return f"Configuration error: {str(self.msg)}"
+
+
+class WebAPIException(ArgoSensuToolsException):
+    def __str__(self):
+        return f"Web-API error: {str(self.msg)}"
